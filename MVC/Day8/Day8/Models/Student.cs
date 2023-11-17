88d5249eae5d1c598a7c857c0337bdc1657c768d
Abdelrhmn <abdlrhmntt@gmail.com>
2023-11-17 16:48:11 +0200

    intial Day8 and Added Student and Department Classes

---
+++ MVC/Day8/Day8/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Day8.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public int Age { get; set; }
+        [ForeignKey("Department")]
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+    }
+}
