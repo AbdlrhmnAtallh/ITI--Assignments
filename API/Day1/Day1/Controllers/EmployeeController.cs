8d39173af23400d352b29e937fcd0498efdded06
Abdelrhmn <abdlrhmntt@gmail.com>
2024-06-27 09:06:41 +0300

    chore: day1 is completed

---
+++ API/Day1/Day1/Controllers/EmployeeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Day1.Models;
+namespace Day1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public static List<Employee> Employees = new List<Employee>();
+
+        // POST api/Employee
+        [HttpPost]
+        public IActionResult Add([FromBody] Employee employee)
+        {
+            Employees.Add(employee);
+            return Ok("Employee has been added");
+        }
+
+        // GET api/Employee/1
+        [HttpGet]
+        [Route("{id:int}")]
+        public IActionResult GetEmployeeById(int id)
+        {
+            var e = Employees.FirstOrDefault(e => e.Id == id);
+            if (e == null)
+            {
+                return BadRequest("No id has matched");
+            }
+            return Ok(e); // Ok is a status code in head and e is json code in body
+        }
+
+        // GET api/Employee/name/John
+        [HttpGet]
+        [Route("name/{name:alpha}")]
+        public IActionResult GetEmployeeByName([FromRoute] string name)
+        {
+            Employee e = Employees.FirstOrDefault(e => e.Name == name);
+            if (e == null)
+            {
+                return BadRequest("No Employee has this name");
+            }
+            return Ok(e);
+        }
+
+        // GET api/Employee/all
+        [HttpGet]
+        [Route("all")]
+        public IActionResult GetAll()
+        {
+            return Ok(Employees.ToList());
+        }
+    }
+}
+
---
+++ API/Day1/Day1/Models/Day1dbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Day1.Models
+{
+    public class Day1dbContext
+    {
+        public static List<Employee> Employees { get; set; }
+        
+        public Day1dbContext()
+        {
+            Employee item = new Employee();
+            item.Id = 1; item.Name = "A"; item.salary = 100;
+            Employees.Add(item);
+        }
+    }
+}
